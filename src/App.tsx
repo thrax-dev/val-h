fd7ab34cce21314e789ce1a316a473170a2c79f4
Thrax Dev <thrax.dev@gmail.com>
2023-02-24 22:36:34 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <h1 className="text-4xl text-green-500 font-bold tracking-wider">personal space of val-h</h1>
+    </div>
+  )
+}
+
+export default App;
