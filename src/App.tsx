da224398c5f8479960105908657bbce6741f0d22
Thrax Dev <thrax.dev@gmail.com>
2023-02-25 00:11:55 +0200

    dev branch init, htaccess file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <h1 className="text-4xl text-green-500 font-bold tracking-wider">personal space of val-h</h1>
+      <h1 className="text-2xl text-green-500 font-bold tracking-wider">personal space of val-h</h1>
     </div>
   )
 }
